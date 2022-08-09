4b9a10acb99f838fbe5fdca79458aa7368035a62
Timur <tumyp.paxumob@gmail.com>
2022-08-09 22:34:56 +0500

    Added file: TaskCode

---
+++ TaskCode/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// МЕТОДЫ:
+
+void GetArray(string[] array) // Метод ввода элементов массива с консоли
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Input {i + 1} array element: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+
