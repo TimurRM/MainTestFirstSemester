a5a2d361161861d9cd29ce2b17a42873cba87f29
Timur <tumyp.paxumob@gmail.com>
2022-08-09 22:38:23 +0500

    Added Methods Show and Sort

--- TaskCode/Program.cs
+++ TaskCode/Program.cs
@@ -10,4 +10,44 @@ void GetArray(string[] array) // Метод ввода элементов мас
     }
 }
 
+void ShowArray(string[] array) // Метод вывода элементов массива в консоль
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0)
+        {
+        Console.Write("['" + array[i] + "', '");
+        }
+        else if (i < array.Length - 1)
+        {
+        Console.Write(array[i] + "', '");   
+        }
+        else
+        {
+        Console.Write(array[i] + "']");  
+        }
+        
+    }
+}
+
+
+int SortArray(string[] array) // Метод отсортировки строк в массиве длинной более 3 символов
+{
+    int n = 3;
+    int count = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length > n)
+        {
+        count--;
+        }
+        
+    }
+    return count;
+}
+
+
+Console.Write("Input quantity of array elements: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
 
